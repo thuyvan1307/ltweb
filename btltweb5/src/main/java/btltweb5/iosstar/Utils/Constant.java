0047d3e77bd3b138f021dce00fedf6b7fc70bd85
ICT <ICT@DESKTOP-E736SHO>
2024-10-05 23:47:45 +0700

    upload

--- btltweb5/src/main/java/btltweb5/iosstar/Utils/Constant.java
+++ btltweb5/src/main/java/btltweb5/iosstar/Utils/Constant.java
@@ -3,9 +3,12 @@ package btltweb5.iosstar.Utils;
 public class Constant {
 	public static final String SESSION_USERNAME = "username";
 	public static final String COOKIE_REMEMBER = "username";
-	public static class Path{
-	public static final String REGISTER = "/views/register.jsp";
-	}
-	
-}
 
+	public static class Path {
+		public static final String REGISTER = "/views/register.jsp";
+	}
+
+	public static final String UPLOAD_DIRECTORY = "D:\\upload";
+	public static final String DEFAULT_FILENAME = "default.file";
+
+}
